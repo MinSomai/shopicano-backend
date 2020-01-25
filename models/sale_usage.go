200ca4e52f7499d056c3ae1decf98e05d337b616
Sakib Sami <s4kibs4mi@gmail.com>
2020-01-25 12:04:24 +0600

    - Refactored

--- models/sale_usage.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type SaleUsage struct {
-	SaleID    string `json:"sale_id" gorm:"column:sale_id"`
-	OrderID   string `json:"order_id" gorm:"column:order_id"`
-	ProductID string `json:"product_id" gorm:"column:product_id"`
-}
