3a4943d16688b2e7077d7c13914365e57c514047
Sakib Sami <s4kibs4mi@gmail.com>
2020-02-07 00:39:35 +0600

    - Added : Order Details Email

--- services/send_order_details_email.go
+++ services/send_order_details_email.go
@@ -3,121 +3,45 @@ package services
 import (
 	"fmt"
 	"github.com/go-gomail/gomail"
-	"github.com/matcornic/hermes/v2"
-	"github.com/shopicano/shopicano-backend/app"
 	"github.com/shopicano/shopicano-backend/config"
 	"github.com/shopicano/shopicano-backend/models"
+	"github.com/shopicano/shopicano-backend/templates"
 )
 
 func SendOrderDetailsEmail(name, email string, order *models.OrderDetailsView) error {
-	var entryItems [][]hermes.Entry
+	params := map[string]interface{}{}
+	params["greetings"] = fmt.Sprintf("Hi %s,", order.UserName)
+	params["intros"] = "Thank you for purchasing from our platform."
+	params["orderHash"] = order.Hash
+	params["billingAddress"] = fmt.Sprintf("%s<br>%s<br>%s - %s",
+		order.BillingAddress, order.BillingCity, order.BillingCountry, order.BillingPostcode)
+	params["isShippable"] = !order.IsAllDigitalProducts
+
+	if !order.IsAllDigitalProducts {
+		params["shippingAddress"] = fmt.Sprintf("%s<br>%s<br>%s - %s",
+			*order.ShippingAddress, *order.ShippingCity, *order.ShippingCountry, *order.ShippingPostcode)
+	}
+
+	params["shippingCharge"] = order.ShippingCharge
+	params["paymentProcessingFee"] = order.PaymentProcessingFee
+	params["paymentGateway"] = order.PaymentGateway
+	params["subTotal"] = order.SubTotal
+	params["grandTotal"] = order.GrandTotal
+
+	var items []map[string]interface{}
 
 	for _, v := range order.Items {
-		var rows []hermes.Entry
-		rows = append(rows, hermes.Entry{
-			Key:   "Item",
-			Value: v.Name,
-		})
-		rows = append(rows, hermes.Entry{
-			Key:   "Quantity",
-			Value: fmt.Sprintf("%d", v.Quantity),
-		})
-		rows = append(rows, hermes.Entry{
-			Key:   "Price",
-			Value: fmt.Sprintf("%d", v.Price),
-		})
-		rows = append(rows, hermes.Entry{
-			Key:   "Sub Total",
-			Value: fmt.Sprintf("%d", v.SubTotal),
-		})
-
-		entryItems = append(entryItems, rows)
-	}
-
-	var entryCalculations []hermes.Entry
-
-	entryCalculations = append(entryCalculations, hermes.Entry{
-		Key:   "Status",
-		Value: string(order.Status),
-	})
-
-	entryCalculations = append(entryCalculations, hermes.Entry{
-		Key:   "Payment Status",
-		Value: string(order.PaymentStatus),
-	})
-
-	if !order.PaymentMethodIsOffline {
-		entryCalculations = append(entryCalculations, hermes.Entry{
-			Key:   "Payment Gateway",
-			Value: fmt.Sprintf("%s", order.PaymentGateway),
+		items = append(items, map[string]interface{}{
+			"name":     v.Name,
+			"quantity": v.Quantity,
+			"price":    v.Price,
+			"subTotal": v.SubTotal,
 		})
 	}
 
-	entryCalculations = append(entryCalculations, hermes.Entry{
-		Key:   "Sub Total",
-		Value: fmt.Sprintf("%d", order.SubTotal),
-	})
+	params["orderedItems"] = items
 
-	if order.ShippingCharge != 0 {
-		entryCalculations = append(entryCalculations, hermes.Entry{
-			Key:   "Shipping Charge",
-			Value: fmt.Sprintf("%d", order.ShippingCharge),
-		})
-	}
-
-	if order.PaymentProcessingFee != 0 {
-		entryCalculations = append(entryCalculations, hermes.Entry{
-			Key:   "Payment Processing Fee",
-			Value: fmt.Sprintf("%d", order.PaymentProcessingFee),
-		})
-	}
-
-	entryCalculations = append(entryCalculations, hermes.Entry{
-		Key:   "Grand Total",
-		Value: fmt.Sprintf("%d", order.GrandTotal),
-	})
-
-	emailTemplate := hermes.Email{
-		Body: hermes.Body{
-			Greeting:  "Hello",
-			Signature: "With Love",
-			Name:      name,
-			Intros: []string{
-				"Your order has been placed successfully.<br> Please check order details below,",
-			},
-			Table: hermes.Table{
-				Data: entryItems,
-				Columns: hermes.Columns{
-					CustomWidth: map[string]string{
-						"Item":      "40%",
-						"Price":     "20%",
-						"Quantity":  "15%",
-						"Sub Total": "25%",
-					},
-					CustomAlignment: map[string]string{
-						"Item":      "left",
-						"Price":     "right",
-						"Quantity":  "right",
-						"Sub Total": "right",
-					},
-				},
-			},
-			Dictionary: entryCalculations,
-			Actions: []hermes.Action{
-				{
-					Instructions: "",
-					Button: hermes.Button{
-						Link:      fmt.Sprintf(config.App().PaymentCompleteCallback, order.ID),
-						Color:     "green",
-						Text:      "Order Details",
-						TextColor: "white",
-					},
-				},
-			},
-		},
-	}
-
-	body, err := app.Hermes().GenerateHTML(emailTemplate)
+	body, err := templates.GenerateInvoiceEmailHTML(params)
 	if err != nil {
 		return err
 	}
