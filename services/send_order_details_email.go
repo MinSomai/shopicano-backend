2e573112af233cb1a547d1737da2cf893973a54d
Sakib Sami <s4kibs4mi@gmail.com>
2020-03-15 22:36:14 +0600

    - Updated

--- services/send_order_details_email.go
+++ services/send_order_details_email.go
@@ -23,7 +23,7 @@ func SendOrderDetailsEmail(email, subject string, order *models.OrderDetailsView
 
 	params := map[string]interface{}{}
 	params["greetings"] = fmt.Sprintf("Hi %s,", order.UserName)
-	params["intros"] = "Thank you for purchasing from our platform."
+	params["intros"] = subject
 	params["orderHash"] = order.Hash
 	params["billingAddress"] = fmt.Sprintf("%s\n%s\n%s - %s",
 		order.BillingAddress, order.BillingCity, order.BillingCountry, order.BillingPostcode)
@@ -103,102 +103,7 @@ func SendOrderDetailsEmail(email, subject string, order *models.OrderDetailsView
 		return tasks.NewErrRetryTaskLater(err.Error(), TaskRetryDelay)
 	}
 
-	m := gomail.NewMessage()
-	m.SetHeader("From", fmt.Sprintf("%s", config.EmailService().FromEmailAddress))
-	m.SetHeader("To", email)
-	m.SetHeader("Subject", subject)
-	m.SetBody("text/html", body)
-
-	if err := EmailDialer().DialAndSend(m); err != nil {
-		log.Log().Errorln(err)
-		return tasks.NewErrRetryTaskLater(err.Error(), TaskRetryDelay)
-	}
-	return nil
-}
-
-func SendPaymentConfirmationEmail(email string, order *models.OrderDetailsView) error {
-	params := map[string]interface{}{}
-	params["greetings"] = fmt.Sprintf("Hi %s,", order.UserName)
-	params["intros"] = "Thank you for the payment."
-	params["orderHash"] = order.Hash
-	params["billingAddress"] = fmt.Sprintf("%s\n%s\n%s - %s",
-		order.BillingAddress, order.BillingCity, order.BillingCountry, order.BillingPostcode)
-	params["isShippable"] = !order.IsAllDigitalProducts
-
-	if !order.IsAllDigitalProducts {
-		params["shippingAddress"] = fmt.Sprintf("%s\n%s\n%s - %s",
-			*order.ShippingAddress, *order.ShippingCity, *order.ShippingCountry, *order.ShippingPostcode)
-	}
-
-	params["shippingCharge"] = fmt.Sprintf("%.2f", float64(order.ShippingCharge)/100)
-	params["paymentProcessingFee"] = fmt.Sprintf("%.2f", float64(order.PaymentProcessingFee)/100)
-	params["subTotal"] = fmt.Sprintf("%.2f", float64(order.SubTotal)/100)
-	params["grandTotal"] = fmt.Sprintf("%.2f", float64(order.GrandTotal)/100)
-	params["isCouponApplied"] = false
-
-	pg, err := payment_gateways.GetPaymentGatewayByName(order.PaymentGateway)
-	if err != nil {
-		params["paymentGateway"] = "None"
-	} else {
-		params["paymentGateway"] = pg.DisplayName()
-	}
-
-	switch order.Status {
-	case models.OrderPending:
-		params["status"] = "Pending"
-	case models.OrderConfirmed:
-		params["status"] = "Confirmed"
-	case models.OrderCancelled:
-		params["status"] = "Cancelled"
-	case models.OrderShipping:
-		params["status"] = "Shipping"
-	case models.OrderDelivered:
-		params["status"] = "Delivered"
-	}
-
-	switch order.PaymentStatus {
-	case models.PaymentPending:
-		params["paymentStatus"] = "Pending"
-	case models.PaymentCompleted:
-		params["paymentStatus"] = "Completed"
-	case models.PaymentFailed:
-		params["paymentStatus"] = "Failed"
-	case models.PaymentReverted:
-		params["paymentStatus"] = "Reverted"
-	}
-
-	if order.DiscountedAmount != 0 {
-		params["couponCode"] = order.CouponCode
-		params["discount"] = fmt.Sprintf("%.2f", float64(order.DiscountedAmount)/100)
-		params["isCouponApplied"] = true
-	}
-
-	var items []map[string]interface{}
-
-	for _, v := range order.Items {
-		items = append(items, map[string]interface{}{
-			"name":     v.Name,
-			"quantity": v.Quantity,
-			"price":    fmt.Sprintf("%.2f", float64(v.Price)/100),
-			"subTotal": fmt.Sprintf("%.2f", float64(v.SubTotal)/100),
-		})
-	}
-
-	params["orderedItems"] = items
-
-	body, err := templates.GenerateInvoiceEmailHTML(params)
-	if err != nil {
-		log.Log().Errorln(err)
-		return tasks.NewErrRetryTaskLater(err.Error(), TaskRetryDelay)
-	}
-
-	m := gomail.NewMessage()
-	m.SetHeader("From", fmt.Sprintf("%s", config.EmailService().FromEmailAddress))
-	m.SetHeader("To", email)
-	m.SetHeader("Subject", "Thank you for the payment")
-	m.SetBody("text/html", body)
-
-	if err := EmailDialer().DialAndSend(m); err != nil {
+	if err := SendEmail(subject, email, body); err != nil {
 		log.Log().Errorln(err)
 		return tasks.NewErrRetryTaskLater(err.Error(), TaskRetryDelay)
 	}
