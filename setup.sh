5739da8dcc9bc32ae10e6d7c9ca798d092bd6785
Sakib Sami <s4kibs4mi@gmail.com>
2020-03-24 22:53:08 +0600

    - Update

--- setup.sh
+++ setup.sh
@@ -1,63 +1,13 @@
 #!/bin/bash
 
-export CONSUL_URL="128.199.89.247:8500"
+export CONSUL_URL="0.0.0.0:8500"
 export CONSUL_PATH="shopicano"
 
-export GO111MODULE=on
-export GOARCH="amd64"
-export CGO_ENABLED=0
+echo "Starting shopicano setup..."
+echo "Starting docker..."
+docker-compose up
+echo "Docker is up"
 
-cmd=$1
-
-binary="shopicano"
-
-########## Dep Hack
-go get github.com/ugorji/go@v1.1.2-0.20180831062425-e253f1f20942
-##########
-
-if [ "$cmd" = "build" ]; then
-  echo "Executing build command"
-  go get .
-  go build -v -o ${binary}
-  exit
-fi
-
-if [ "$cmd" = "run" ]; then
-  echo "Executing run command"
-  #  curl --request PUT --data-binary @config.yml http://localhost:8500/v1/kv/${CONSUL_PATH}
-  ./${binary} serve
-  exit
-fi
-
-if [ "$cmd" = "worker" ]; then
-  echo "Executing worker command"
-  #  curl --request PUT --data-binary @config.yml http://localhost:8500/v1/kv/${CONSUL_PATH}
-  ./${binary} worker
-  exit
-fi
-
-if [ "$cmd" = "auto" ]; then
-  echo "Executing migration auto command"
-  ./${binary} migration auto
-  exit
-fi
-
-if [ "$cmd" = "init" ]; then
-  echo "Executing migration init command"
-  ./${binary} migration init
-  exit
-fi
-
-if [ "$cmd" = "drop" ]; then
-  echo "Executing migration drop command"
-  ./${binary} migration drop
-  exit
-fi
-
-if [ "$cmd" = "docker" ]; then
-  echo "Executing docker build command"
-  docker build -t docker.pkg.github.com/shopicano/shopicano-backend/shopicano-backend:"$2" .
-  exit
-fi
-
-echo "No command specified"
+echo "Configuring environment..."
+curl --request PUT --data-binary @config.example.yml http://${CONSUL_URL}/v1/kv/${CONSUL_PATH}
+echo "Shopicano is started"
