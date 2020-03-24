0ec98c6ea309ecdc25d780f88d6c9326638da0ab
Sakib Sami <s4kibs4mi@gmail.com>
2020-03-24 23:05:54 +0600

    - Update

--- setup.sh
+++ setup.sh
@@ -9,5 +9,19 @@ docker-compose up
 echo "Docker is up"
 
 echo "Configuring environment..."
+
+echo "Adding default config to consul..."
 curl --request PUT --data-binary @config.example.yml http://${CONSUL_URL}/v1/kv/${CONSUL_PATH}
-echo "Shopicano is started"
+
+echo "Finding shopicano docker container..."
+containers=$(docker ps | grep shopicano_backend)
+containerInfo=' ' read -r -a array <<<"$containers"
+containerID="${array[0]}"
+
+echo "Running shopicano migration..."
+docker exec "$containerID" shopicano migration auto
+
+echo "Initializing shopicano..."
+docker exec "$containerID" shopicano migration init
+
+echo "Shopicano is ready."
